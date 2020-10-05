a86b47dd5bda8104fbee80418ff019f1d7f88a96
Anthony Avila <21036@columbushs.com>
2020-10-05 11:04:02 -0400

    commit

--- BankAccount.java
+++ BankAccount.java
@@ -1,9 +1,30 @@
 public class BankAccount {
 
     // Instance Variables
-
+private double balance;
     // Constructors
+    public BankAccount();{
+
+        this.balance = 0.0;
+
+    }
+    public BankAccount(double pBalance){
+
+        this.balance = pBalance;
+
+    }
 
     // Methods
+    public void setBalance(double pBalance){
+
+        this.balance = pBalance
+
+    }
+
+    public double getBalance(){
+    
+        return.this.balance
+
+    }
 
 }
--- BankDemo.java
+++ BankDemo.java
@@ -1,10 +1,16 @@
 // Import Scanner Class
+import java.util.Scanner;
 
 public class BankDemo {
 
     public static void main(String[] args) {
         // Initialize Scanner Object
+        Scanner input = new Scanner(System.in);
         // Initialize BankAccount Object
+        BankAccount John = new BankAccount(200.0);
+        System.out.println(john.getBalance());
+        John.setBalance(5000.0);
+
 
     }
 }
